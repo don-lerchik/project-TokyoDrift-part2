e1750b30b748c40164a093dc605f4127efa3920f
Valery <don777lerchik@gmail.com>
2019-01-27 17:24:48 +0300

    make it better2

--- scr/commands/server.js
+++ scr/commands/server.js
@@ -5,7 +5,7 @@ const {LocalServer} = require(`../server/local-server`);
 
 class Server extends AbstractCommand {
   execute() {
-    const HOSTNAME = process.env.HOST || VARIABLES.SERVER_HOST;
+    const HOSTNAME = process.env.HOSTNAME || VARIABLES.SERVER_HOST;
     const PORT = parseInt(process.env.PORT, 10) || VARIABLES.SERVER_PORT;
     const localServer = new LocalServer(HOSTNAME, PORT);
     localServer.start();
